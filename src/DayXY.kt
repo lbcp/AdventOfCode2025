301a5e8f1db0b38e059a2e57da8679d2e679a261
lbcp <michael.fichtner@hotmail.de>
2025-12-05 17:55:03 +0100

    Modified Template .kt file.

--- src/DayXY.kt
+++ src/DayXY.kt
@@ -2,11 +2,7 @@ import kotlin.time.measureTime
 
 fun main() {
     fun parseInput(input: List<String>): MutableList<Pair<Char, Int>> {
-        val parsed = mutableListOf<Pair<Char, Int>>()
-        input.forEach {
-            parsed.add(Pair(it.first(), it.substring(1).toInt()))
-        }
-        return parsed
+        return mutableListOf(Pair('A', 5))
     }
 
     fun part1(input: MutableList<Pair<Char, Int>>): Int {
@@ -19,15 +15,20 @@ fun main() {
         return result
     }
 
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day02_Test")
-    check(part1(parseInput(testInput)) == 3)
-    check(part2(parseInput(testInput)) == 6)
+    val testRawInput = readInput("DayXY_Test")
+    val testInput = parseInput(testRawInput)
 
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day02")
-    val timePart1 = measureTime { part1(parseInput(input)).println() }
+    check(part1(testInput) == 3)
+
+    /*
+    check(part2(testInput) == 6)
+
+    val rawInput = readInput("Day02")
+    val input = parseInput(rawInput)
+    val timePart1 = measureTime { part1(input).println() }
     println("Part 1 took $timePart1.")
-    val timePart2 = measureTime { part2(parseInput(input)).println() }
+    val timePart2 = measureTime { part2(input).println() }
     println("Part 2 took $timePart2.")
+
+     */
 }
