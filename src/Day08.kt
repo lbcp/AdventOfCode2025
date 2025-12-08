3a41692bbe7236715fd82102e681a26f7af1e066
Micha <michael.fichtner@hotmail.de>
2025-12-09 00:31:41 +0100

    Day 8 Part1 completed. For part 2 I need to change my approach slightly.

--- src/Day08.kt
+++ src/Day08.kt
@@ -15,8 +15,34 @@ fun main() {
         return coordinates
     }
 
-    fun part1(input: MutableList<List<Long>>): Long {
-        var result = 0L
+    fun mergeAllOverlapping(circuits: MutableSet<MutableSet<List<Long>>>): MutableSet<MutableSet<List<Long>>> {
+        val remaining = circuits.toMutableSet()
+        val result = mutableSetOf<MutableSet<List<Long>>>()
+        while (remaining.isNotEmpty()) {
+            val start = remaining.first()
+            remaining.remove(start)
+            val merged = start.toMutableSet()
+            var changed = true
+
+            do {
+                changed = false
+                val it = remaining.iterator()
+                while (it.hasNext()) {
+                    val next = it.next()
+                    if (next.any { it in merged }) {
+                        merged.addAll(next)
+                        it.remove()
+                        changed = true
+                    }
+                }
+            } while (changed)
+            result.add(merged)
+        }
+        return result
+    }
+
+
+    fun part1(input: MutableList<List<Long>>, times: Int = 1000): Long {
         // Test to find shortes route
         val prioQueue = PriorityQueue<Double>()
         val connMap = mutableMapOf<Double, Pair<List<Long>, List<Long>>>()
@@ -30,25 +56,43 @@ fun main() {
                 connMap[dist] = Pair(i, input[j])
             }
         }
-        var circuits = mutableListOf< MutableSet<List<Long>>>()
-        addLoop@repeat(10) {
+        val circuits = mutableSetOf<MutableSet<List<Long>>>()
+        repeat(times) {
             val next = prioQueue.poll()
             val (x, y) = connMap[next]!!
-            var timesAdded = 0
-            for (c in circuits) {
-                if (x in c || y in c) {
+            val overlapping = circuits.filter { x in it || y in it }.toMutableSet()
+
+            when (overlapping.size) {
+                0 -> circuits.add(mutableSetOf(x, y))
+                1 -> {
+                    val c = overlapping.first()
                     c.add(x)
                     c.add(y)
-                    timesAdded += 1
+                }
+                else -> {
+                    println("I was added multiple times ${connMap[next]}")
+                    println("Doublets: $overlapping")
+
+                    val merged = overlapping
+                        .flatten()
+                        .toMutableSet()
+                        .apply {
+                            add(x)
+                            add(y)
+                        }
+                    circuits.removeAll(overlapping)
+                    circuits.add(merged)
                 }
             }
-            if (timesAdded == 0) {
-                circuits.add(mutableSetOf(x, y))
-            }
-            if (timesAdded > 1) println("I was added multiple times ${connMap[next]}")
         }
-        // I also must merge them sets in the end
-        var finalSets = mutableListOf< MutableSet<List<Long>>>()
+        val new = mergeAllOverlapping(circuits)
+        println("new")
+        new.forEach { println(it) }
+        val sortedList = new.sortedBy { it.size }
+        sortedList.forEach { println(it) }
+        var result = 1L
+        for (i in sortedList.size-1 downTo sortedList.size-3) result *= sortedList[i].size
+        println(result)
         return result
     }
 
@@ -59,22 +103,18 @@ fun main() {
 
     val testRawInput = readInput("Day08_Test")
     val testInput = parseInput(testRawInput)
-    part1(testInput)
-    /*
+    check(part1(testInput, 10) == 40L)
+
     val rawInput = readInput("Day08")
     val input = parseInput(rawInput)
-    part1(input)
-    //check(part1(testInput) == 40L)
-
-
-     */
+    val timePart1 = measureTime { part1(input).println() }
+    println("Part 1 took $timePart1.")
     /*
     check(part2(testInput) == 6)
 
 
 
-    val timePart1 = measureTime { part1(input).println() }
-    println("Part 1 took $timePart1.")
+
     val timePart2 = measureTime { part2(input).println() }
     println("Part 2 took $timePart2.")
 
