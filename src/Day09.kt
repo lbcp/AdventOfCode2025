2f8a35a384535f1c671f8c8af01f658aadae2748
Micha <michael.fichtner@hotmail.de>
2025-12-09 22:06:11 +0100

    Day9 continued.

--- src/Day09.kt
+++ src/Day09.kt
@@ -33,21 +33,26 @@ fun main() {
             if (x+1 > maxX) maxX = x+1
             if (y+1 > maxY) maxY = y+1
         }
-        println("MaxX = $maxX")
-        println("MaxY = $maxY")
         val row = mutableListOf<Char>()
         val grid = mutableListOf<MutableList<Char>>()
         repeat(maxX.toInt()+1) { row.add('.') }
         repeat(maxY.toInt()+1) { grid.add(row.toMutableList()) }
-        grid.forEach { println(it) }
 
         for ((no, item) in input.withIndex()) {
             val (x, y) = item
-            println(item)
+            grid[y.toInt()][x.toInt()] = 'X'
+            val (x2, y2) =  if (no + 1 >= input.size) input.first() else input[no+1]
+                when {
+                    x < x2 -> for (i in x+1..x2) grid[y.toInt()][i.toInt()] = 'O'
+                    x > x2 -> for (i in x2+1..x) grid[y.toInt()][i.toInt()] = 'O'
+                    y < y2 -> for (i in y+1..y2) grid[i.toInt()][x.toInt()] = 'O'
+                    y > y2 -> for (i in y2+1..y) grid[i.toInt()][x.toInt()] = 'O'
+                }
             grid[y.toInt()][x.toInt()] = 'X'
         }
         grid.forEach { println(it) }
 
+        // Flood fill with Os
         return grid
     }
 
