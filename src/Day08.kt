569c6e7baf8557a6acf95e5c5d1ffe7b1f657174
lbcp <michael.fichtner@hotmail.de>
2025-12-09 13:57:15 +0100

    Day 8 Part 2 completed.

--- src/Day08.kt
+++ src/Day08.kt
@@ -1,5 +1,4 @@
 import java.util.PriorityQueue
-import kotlin.math.exp
 import kotlin.math.pow
 import kotlin.math.sqrt
 import kotlin.time.measureTime
@@ -22,7 +21,7 @@ fun main() {
             val start = remaining.first()
             remaining.remove(start)
             val merged = start.toMutableSet()
-            var changed = true
+            var changed: Boolean
 
             do {
                 changed = false
@@ -43,7 +42,6 @@ fun main() {
 
 
     fun part1(input: MutableList<List<Long>>, times: Int = 1000): Long {
-        // Test to find shortes route
         val prioQueue = PriorityQueue<Double>()
         val connMap = mutableMapOf<Double, Pair<List<Long>, List<Long>>>()
         for ((no1, i) in input.withIndex()) {
@@ -70,9 +68,6 @@ fun main() {
                     c.add(y)
                 }
                 else -> {
-                    println("I was added multiple times ${connMap[next]}")
-                    println("Doublets: $overlapping")
-
                     val merged = overlapping
                         .flatten()
                         .toMutableSet()
@@ -86,19 +81,57 @@ fun main() {
             }
         }
         val new = mergeAllOverlapping(circuits)
-        println("new")
-        new.forEach { println(it) }
         val sortedList = new.sortedBy { it.size }
-        sortedList.forEach { println(it) }
         var result = 1L
         for (i in sortedList.size-1 downTo sortedList.size-3) result *= sortedList[i].size
-        println(result)
         return result
     }
 
-    fun part2(input: MutableList<Pair<Char, Int>>): Int {
-        var result = 0
-        return result
+    fun part2(input: MutableList<List<Long>>): Long {
+        val nodesCount = input.size
+        println(nodesCount)
+        val prioQueue = PriorityQueue<Double>()
+        val connMap = mutableMapOf<Double, Pair<List<Long>, List<Long>>>()
+        for ((no1, i) in input.withIndex()) {
+            for (j in no1 + 1..<input.size) {
+                if (i == input[j]) continue
+                val dist = sqrt((i[0].toDouble() - input[j][0]).pow(2) +
+                        (i[1].toDouble() - input[j][1]).pow(2) +
+                        (i[2].toDouble() - input[j][2]).pow(2))
+                prioQueue.add(dist)
+                connMap[dist] = Pair(i, input[j])
+            }
+        }
+        var circuits = mutableSetOf<MutableSet<List<Long>>>()
+        var lastPair: Pair<List<Long>, List<Long>>
+        do {
+            val next = prioQueue.poll()
+            val (x, y) = connMap[next]!!
+            val overlapping = circuits.filter { x in it || y in it }.toMutableSet()
+
+            when (overlapping.size) {
+                0 -> circuits.add(mutableSetOf(x, y))
+                1 -> {
+                    val c = overlapping.first()
+                    c.add(x)
+                    c.add(y)
+                }
+                else -> {
+                    val merged = overlapping
+                        .flatten()
+                        .toMutableSet()
+                        .apply {
+                            add(x)
+                            add(y)
+                        }
+                    circuits.removeAll(overlapping)
+                    circuits.add(merged)
+                }
+            }
+            circuits = mergeAllOverlapping(circuits)
+            lastPair = Pair(x, y)
+        } while (circuits.first().size < nodesCount)
+        return lastPair.first[0] * lastPair.second[0]
     }
 
     val testRawInput = readInput("Day08_Test")
@@ -107,16 +140,13 @@ fun main() {
 
     val rawInput = readInput("Day08")
     val input = parseInput(rawInput)
-    val timePart1 = measureTime { part1(input).println() }
-    println("Part 1 took $timePart1.")
-    /*
-    check(part2(testInput) == 6)
-
-
-
-
-    val timePart2 = measureTime { part2(input).println() }
-    println("Part 2 took $timePart2.")
-
-     */
+    repeat(5) {
+        val timePart1 = measureTime { part1(input).println() }
+        println("Part 1 took $timePart1.")
+    }
+    check(part2(testInput) == 25272L)
+    repeat(5) {
+        val timePart2 = measureTime { part2(input).println() }
+        println("Part 2 took $timePart2.")
+    }
 }
