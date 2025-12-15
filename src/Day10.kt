94ef4728d1107a42e679c7409b12730d3b881bca
Micha <michael.fichtner@hotmail.de>
2025-12-15 22:42:05 +0100

    Day 10 Part 1 done.

--- src/Day10.kt
+++ src/Day10.kt
@@ -3,8 +3,8 @@ import kotlin.math.pow
 import kotlin.time.measureTime
 
 fun main() {
-    fun parseInput(input: List<String>): MutableList<Triple<List<Char>, List<List<Int>>, List<Int>>> {
-        val parsed = mutableListOf<Triple<List<Char>, List<List<Int>>, List<Int>>>()
+    fun parseInput(input: List<String>): MutableList<Triple<Int, List<Int>, List<Int>>> {
+        val parsed = mutableListOf<Triple<Int, List<Int>, List<Int>>>()
         input.forEach {
             val levers = """\s.+\s""".toRegex()
             val endLayout = """\[.+]""".toRegex()
@@ -23,27 +23,47 @@ fun main() {
                 .split(",")
                 .map { nums -> nums.toInt() }
 
-            val leversAsNumbers = mutableListOf<Double>()
+            val leversAsNumbers = mutableListOf<Int>()
 
             for (parts in extractedLevers) {
                 var foo = 0.0
                 val temp = parts.split(",").map { num -> num.toInt() }
-                for ((i, j) in temp.withIndex()) {
+                for (j in temp) {
                     foo += 2.0.toFloat().pow(layout.length-1-j)
                 }
-                leversAsNumbers.add(foo)
-                println(foo.toLong().toString(2).padStart(layout.length, '0'))
+                leversAsNumbers.add(foo.toInt())
             }
-
-            println(leversAsNumbers)
-
+            var finalNumber = 0.0
+            for ((i, j) in layout.reversed().withIndex()) {
+                if (j == '#') finalNumber += 2.0.pow(i)
+            }
+            parsed.add(Triple(finalNumber.toInt(), leversAsNumbers, jolts))
         }
-
         return parsed
     }
 
-    fun part1(input: MutableList<Pair<Char, Int>>): Int {
+    fun part1(input: MutableList<Triple<Int, List<Int>, List<Int>>>): Int {
         var result = 0
+        input.forEach {
+            val resultDict = mutableMapOf<Int, MutableList<Int>>(0 to mutableListOf<Int>())
+            val (finalNumber, buttons, jolts) = it
+            do {
+                val tempDict = mutableMapOf<Int, MutableList<Int>>()
+                for ((key, value) in resultDict) {
+                    for (button in buttons) {
+                        val newVal = key xor button
+                        if (resultDict.containsKey(newVal)) {
+                            continue
+                        } else {
+                            val temp = (value + button).toMutableList()
+                            tempDict[newVal] = temp
+                        }
+                    }
+                }
+                resultDict += tempDict
+            } while (!resultDict.containsKey(finalNumber))
+            result += resultDict[finalNumber]!!.size
+        }
         return result
     }
 
@@ -55,15 +75,17 @@ fun main() {
     val testRawInput = readInput("Day10_Test")
     val testInput = parseInput(testRawInput)
 
-    // check(part1(testInput) == 3)
+    check(part1(testInput) == 7)
+
+    val rawInput = readInput("Day10")
+    val input = parseInput(rawInput)
+    val timePart1 = measureTime { part1(input).println() }
+    println("Part 1 took $timePart1.")
 
     /*
     check(part2(testInput) == 6)
 
-    val rawInput = readInput("Day02")
-    val input = parseInput(rawInput)
-    val timePart1 = measureTime { part1(input).println() }
-    println("Part 1 took $timePart1.")
+
     val timePart2 = measureTime { part2(input).println() }
     println("Part 2 took $timePart2.")
 
